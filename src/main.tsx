a679e066c80b5a737c15012585a71fe92cc8c5b4
gabsvelozo <gabsmastellari@gmail.com>
2025-05-25 17:07:15 -0300

    adicionando o CartProvider

--- src/main.tsx
+++ src/main.tsx
@@ -4,14 +4,17 @@ import './index.css';
 import App from './App.tsx';
 import Navbar from './components/Navbar.tsx';
 import { BrowserRouter } from 'react-router-dom';
+import { CartProvider } from './contexts/CartContext';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      <div className="font-aileron">
-        <Navbar />
-        <App />
-      </div>
+      <CartProvider>
+        <div className="font-aileron">
+          <Navbar />
+          <App />
+        </div>
+      </CartProvider>
     </BrowserRouter>
   </StrictMode>
 );
