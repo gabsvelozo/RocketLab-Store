19f87b1d968cdac8530f2f411a5c5933935ada0b
gabsvelozo <gabsmastellari@gmail.com>
2025-05-25 23:15:14 -0300

    adicionando pagina de detalhes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import Hero from "./components/Hero";
 import ProdutosPage from "./pages/ProdutosPage";
+import ProdutoDetailsPage from "./pages/ProdutoDetailsPage"; 
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Hero />} />
       <Route path="/produtos" element={<ProdutosPage />} />
+      <Route path="/produto/:id" element={<ProdutoDetailsPage />} /> {/* Nova rota */}
     </Routes>
   );
 }
