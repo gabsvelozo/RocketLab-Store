05727cef0fde9f158f1100a111ab850111ed546f
gabsvelozo <gabsmastellari@gmail.com>
2025-05-25 23:15:54 -0300

    card ajustado

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import type { Produto } from "../models/produtos";
 
 interface CardProps {
@@ -8,23 +9,41 @@ interface CardProps {
 const Card = ({ produto, onAddToCart }: CardProps) => {
   return (
     <div className="bg-[#f2f6ef] max-w-[400px] shadow-lg p-4 border rounded-xl flex flex-col justify-between hover:shadow-xl transition-shadow">
-      <img
-        src={produto.imagens[0]}
-        alt={produto.nome}
-        className="w-full h-[390px] object-contain rounded-xl mb-4 bg-[#e4ece0]"
-      />
+      <Link 
+        to={`/produto/${produto.id}`}
+        state={{ produto }} 
+        className="block"
+      >
+        <img
+          src={produto.imagens[0]}
+          alt={produto.nome}
+          className="w-full h-[390px] object-contain rounded-xl mb-4 bg-[#e4ece0] hover:opacity-90 transition-opacity"
+        />
+      </Link>
       
       <div className="flex justify-between items-center">
         <div>
-          <h2 className="text-xl font-ivymode font-regular text-black">{produto.nome}</h2>
+          <Link 
+            to={`/produto/${produto.id}`}
+            state={{ produto }} 
+            className="hover:underline"
+          >
+            <h2 className="text-xl font-ivymode font-regular text-black hover:text-gray-700">
+              {produto.nome}
+            </h2>
+          </Link>
           <p className="text-lg text-green-800 font-medium mt-1">
             R${produto.preco.toFixed(2)}
           </p>
         </div>
 
         <button
-          onClick={() => onAddToCart?.(produto)}
-          className="bg-[#889e86] hover:bg-gray-400 text-white text-sm font-aileron px-4 py-2 rounded transition-all"
+          onClick={(e) => {
+            e.preventDefault();
+            e.stopPropagation();
+            onAddToCart?.(produto);
+          }}
+          className="bg-[#5d6d5c] hover:bg-gray-400 text-white text-sm font-aileron px-4 py-2 rounded transition-all"
         >
           Adicionar ao Carrinho
         </button>
