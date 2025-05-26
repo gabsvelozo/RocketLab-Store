84f17850fec1fe39ae46dc184f36af8776b02807
gabsvelozo <gabsmastellari@gmail.com>
2025-05-26 19:04:34 -0300

    modificacoes card

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -8,31 +8,23 @@ interface CardProps {
 
 const Card = ({ produto, onAddToCart }: CardProps) => {
   return (
-    <div className="bg-[#f2f6ef] max-w-[400px] shadow-lg p-4 border rounded-xl flex flex-col justify-between hover:shadow-xl transition-shadow">
-      <Link 
-        to={`/produto/${produto.id}`}
-        state={{ produto }} 
-        className="block"
-      >
+    <div className="bg-[#f2f6ef] w-full max-w-[400px] shadow-lg p-4 border rounded-xl flex flex-col justify-between hover:shadow-xl transition-shadow">
+      <Link to={`/produto/${produto.id}`} state={{ produto }} className="block">
         <img
           src={produto.imagens[0]}
           alt={produto.nome}
-          className="w-full h-[390px] object-contain rounded-xl mb-4 bg-[#e4ece0] hover:opacity-90 transition-opacity"
+          className="w-full h-[200px] sm:h-[300px] md:h-[390px] object-contain rounded-xl mb-4 bg-[#e4ece0] hover:opacity-90 transition-opacity"
         />
       </Link>
       
-      <div className="flex justify-between items-center">
-        <div>
-          <Link 
-            to={`/produto/${produto.id}`}
-            state={{ produto }} 
-            className="hover:underline"
-          >
-            <h2 className="text-xl font-ivymode font-regular text-black hover:text-gray-700">
+      <div className="flex flex-col sm:flex-row sm:justify-between sm:items-center gap-2">
+        <div className="flex-1">
+          <Link to={`/produto/${produto.id}`} state={{ produto }} className="hover:underline">
+            <h2 className="text-lg sm:text-xl font-ivymode font-regular text-black hover:text-gray-700">
               {produto.nome}
             </h2>
           </Link>
-          <p className="text-lg text-green-800 font-medium mt-1">
+          <p className="text-md sm:text-lg text-green-800 font-medium mt-1">
             R${produto.preco.toFixed(2)}
           </p>
         </div>
@@ -43,7 +35,7 @@ const Card = ({ produto, onAddToCart }: CardProps) => {
             e.stopPropagation();
             onAddToCart?.(produto);
           }}
-          className="bg-[#6a8268] hover:bg-[#889e86] text-white text-sm font-aileron px-4 py-2 rounded transition-all"
+          className="bg-[#6a8268] hover:bg-[#889e86] text-white text-sm font-aileron px-4 py-2 rounded transition-all mt-2 sm:mt-0"
         >
           Adicionar ao Carrinho
         </button>
